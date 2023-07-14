c521bc6e4767a7f88db266f0f8d7b9c5c43db0a2
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-14 12:36:24 +0300

    refactor: remove firebase import from `main.tsx`

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { initializeApp } from "firebase/app";
 
 import App from "./App.tsx";
 
 import "./index.css";
-
-import { FIREBASE_CONFIG } from "./constants/firebaseConfig.ts";
-
-const firebaseApp = initializeApp(FIREBASE_CONFIG);
-
-console.log(firebaseApp);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
