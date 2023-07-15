6eeb6eaf38863dba62fc8c4d99d4e55206f79d46
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-15 20:21:49 +0300

    feat(features): add userSlice

---
+++ src/features/user/userSlice.ts
@@ -0,0 +1,27 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { User } from "firebase/auth";
+
+export interface UserState {
+  userData: User | null;
+}
+
+const initialState: UserState = {
+  userData: null,
+};
+
+export const counterSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      state.userData = action.payload;
+    },
+    resetUser: (state) => {
+      state.userData = null;
+    },
+  },
+});
+
+export const { setUser, resetUser } = counterSlice.actions;
+
+export default counterSlice.reducer;
