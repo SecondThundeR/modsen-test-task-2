7dc611e424d97eccd6064b77b4b92115fc9b0ca8
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-15 23:01:43 +0300

    feat(hooks): update login/signup hooks
    - Add state for error
    - Add update of displayName after signup
    - Extract form data from event

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,27 +1,33 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
 import { FormEvent, useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 import { ROUTES } from "@/constants/routes";
 
 import { auth } from "@/services/firebase";
 
-export function useLogin(email: string, password: string) {
+export function useLogin() {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
 
   const onSubmit = useCallback(
     (event: FormEvent<HTMLFormElement>) => {
       event.preventDefault();
+      setError(null);
       setIsLoading(true);
+
+      const formData = new FormData(event.currentTarget);
+      const email = formData.get("email") as string;
+      const password = formData.get("password") as string;
 
       signInWithEmailAndPassword(auth, email, password)
         .then(() => navigate(ROUTES.home))
-        .catch(console.error)
+        .catch(setError)
         .finally(() => setIsLoading(false));
     },
-    [email, password, navigate, setIsLoading]
+    [navigate, setError, setIsLoading]
   );
 
-  return { isLoading, onSubmit };
+  return { isLoading, error, onSubmit };
 }
--- src/hooks/useSignup.ts
+++ src/hooks/useSignup.ts
@@ -1,27 +1,39 @@
 import { FormEvent, useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 import { ROUTES } from "@/constants/routes";
 
 import { auth } from "@/services/firebase";
 
-export function useSignup(email: string, password: string) {
+export function useSignup() {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
 
   const onSubmit = useCallback(
     async (event: FormEvent<HTMLFormElement>) => {
       event.preventDefault();
+      setError(null);
       setIsLoading(true);
 
+      const formData = new FormData(event.currentTarget);
+      const email = formData.get("email") as string;
+      const password = formData.get("password") as string;
+      const displayName = formData.get("displayName") as string;
+
       createUserWithEmailAndPassword(auth, email, password)
+        .then((credentials) =>
+          updateProfile(credentials.user, {
+            displayName,
+          })
+        )
         .then(() => navigate(ROUTES.home))
-        .catch(console.error)
+        .catch(setError)
         .finally(() => setIsLoading(false));
     },
-    [email, password, navigate, setIsLoading]
+    [navigate, setError, setIsLoading]
   );
 
-  return { isLoading, onSubmit };
+  return { isLoading, error, onSubmit };
 }
