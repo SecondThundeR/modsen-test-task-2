90e05303ef57856d6352e32bab5d197fd4bd7aa3
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-16 21:42:51 +0300

    feat(components): update Home
    Add support for Map component
    Add floating header to the left of the screen

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -2,6 +2,8 @@ import { useEffect, useCallback } from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { Link } from "react-router-dom";
 
+import { Map } from "@/components/Map";
+
 import { resetUser, setUser } from "@/features/user/userSlice";
 
 import { useAppSelector } from "@/hooks/redux/useAppSelector";
@@ -30,32 +32,30 @@ export const Home = () => {
   }, []);
 
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there!</h1>
-
-          <p className="py-6">
-            {user
-              ? `You are currently logged in as "${user.displayName}"`
-              : "To continue, please login or signup"}
-          </p>
-          {user ? (
-            <button className="btn btn-primary" onClick={handleLogout}>
-              Log out
-            </button>
-          ) : (
-            <div className="flex gap-2 justify-center">
-              <Link to="/login" className="btn btn-primary">
-                Login
-              </Link>
-              <Link to="/signup" className="btn btn-accent">
-                Signup
-              </Link>
-            </div>
-          )}
-        </div>
-      </div>
+    <div className="min-h-screen">
+      <header className="w-1/6 flex flex-col p-4 drop-shadow-2xl z-10 fixed top-0 left-0 bg-base-200 h-full">
+        <h1 className="text-2xl font-bold">Hello there!</h1>
+        <p className="py-6 flex-grow">
+          {user
+            ? `You are currently logged in as "${user.displayName}"`
+            : "To continue, please login or signup"}
+        </p>
+        {user ? (
+          <button className="btn btn-primary" onClick={handleLogout}>
+            Log out
+          </button>
+        ) : (
+          <div className="flex gap-2">
+            <Link to="/login" className="btn btn-primary flex-grow">
+              Login
+            </Link>
+            <Link to="/signup" className="btn btn-accent flex-grow">
+              Signup
+            </Link>
+          </div>
+        )}
+      </header>
+      <Map />
     </div>
   );
 };
