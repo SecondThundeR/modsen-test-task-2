23995e8a942baa004797a1e8d703b8005cb14746
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-17 22:27:45 +0300

    chore: update dependencies
    Add leaflet, react-leaflet and @types/leaflet
    Remove @pbe/react-yandex-maps

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
-import { YMaps } from "@pbe/react-yandex-maps";
 
 import { router } from "@/routes";
 import { store } from "@/store";
@@ -8,9 +7,7 @@ import { store } from "@/store";
 function App() {
   return (
     <Provider store={store}>
-      <YMaps>
-        <RouterProvider router={router} />
-      </YMaps>
+      <RouterProvider router={router} />
     </Provider>
   );
 }
