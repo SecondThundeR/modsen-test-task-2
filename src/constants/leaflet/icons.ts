dfe2618e4529f0091fc280ea64e195539d0e2d53
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-17 23:51:33 +0300

    feat(constants): add custom markers for leaflet
    Add icon for user and accommodation markers

---
+++ src/constants/leaflet/icons.ts
@@ -0,0 +1,11 @@
+import { icon } from "leaflet";
+
+export const userIcon = icon({
+  iconUrl: "./src/assets/markerIcons/userIcon.png",
+  iconSize: [24, 24],
+});
+
+export const accommodationIcon = icon({
+  iconUrl: "./src/assets/markerIcons/accommodationIcon.png",
+  iconSize: [48, 48],
+});
