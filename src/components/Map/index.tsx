d1975be6fa89371a9641cacf4173f3fc71e46c33
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-17 23:55:49 +0300

    feat(components): update Map
    Update UserLocation function:
    - Add places fetch to show with markers
    - Add LayerGroup for user marker
    - Use custom marker icons
    - Remove popup from user marker
    - Rename to MapContents
    Change map zoom to 18

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,12 +1,28 @@
-import { LatLng } from "leaflet";
 import { useEffect, useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { LatLng } from "leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  LayerGroup,
+  Circle,
+} from "react-leaflet";
+
+import { accommodationIcon, userIcon } from "@/constants/leaflet/icons";
+
+import { PlacesProperties } from "@/schemas/geoapify";
+
+import { getCurrentCityID } from "@/services/geoapify/getCurrentCityID";
+import { getCurrentCityPlaces } from "@/services/geoapify/getCurrentCityPlaces";
 
 const FALLBACK_LATITUDE = 51.505;
 const FALLBACK_LONGITUDE = -0.09;
 
-function UserLocation() {
+function MapContents() {
   const [position, setPosition] = useState<LatLng | null>();
+  const [places, setPlaces] = useState<PlacesProperties>();
   const map = useMap();
 
   map.addEventListener("locationfound", (event) => {
@@ -21,12 +37,32 @@ function UserLocation() {
     });
   }, [map]);
 
+  useEffect(() => {
+    if (!position) return;
+    getCurrentCityID(position.lat, position.lng)
+      .then(getCurrentCityPlaces)
+      .then(setPlaces)
+      .catch(console.error);
+  }, [position]);
+
   if (!position) return null;
 
   return (
-    <Marker position={position}>
-      <Popup>You are currently here!</Popup>
-    </Marker>
+    <>
+      <LayerGroup>
+        <Marker position={position} icon={userIcon} />
+        <Circle center={position} radius={30} />
+      </LayerGroup>
+
+      {places?.map((place) => {
+        const { lat, lon, name, address_line2, place_id } = place.properties;
+        return (
+          <Marker key={place_id} position={[lat, lon]} icon={accommodationIcon}>
+            <Popup>{`${name} (${address_line2})`}</Popup>
+          </Marker>
+        );
+      })}
+    </>
   );
 }
 
@@ -34,7 +70,7 @@ export function Map() {
   return (
     <MapContainer
       center={[FALLBACK_LATITUDE, FALLBACK_LONGITUDE]}
-      zoom={13}
+      zoom={18}
       scrollWheelZoom={true}
       className="h-screen w-full z-0"
     >
@@ -42,7 +78,7 @@ export function Map() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <UserLocation />
+      <MapContents />
     </MapContainer>
   );
 }
