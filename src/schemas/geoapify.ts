194afe5e31c86266f676bd5691ce63d9eb5281f6
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-18 23:55:13 +0300

    feat(schemas): update PlacesSchema
    Add categories array to place properties
    Add PlaceProperties type
    Move out PlaceProperties

--- src/schemas/geoapify.ts
+++ src/schemas/geoapify.ts
@@ -8,18 +8,22 @@ export const GeocodeReverseSchema = z.object({
   ),
 });
 
+const PlaceProperties = z.object({
+  lat: z.number(),
+  lon: z.number(),
+  name: z.string().nullish(),
+  place_id: z.string(),
+  address_line2: z.string(),
+  categories: z.array(z.string()),
+});
+
 export const PlacesSchema = z.object({
   features: z.array(
     z.object({
-      properties: z.object({
-        lat: z.number(),
-        lon: z.number(),
-        name: z.string().nullish(),
-        place_id: z.string(),
-        address_line2: z.string(),
-      }),
+      properties: PlaceProperties,
     })
   ),
 });
 
+export type PlacePropeties = z.infer<typeof PlaceProperties>;
 export type PlacesProperties = z.infer<typeof PlacesSchema>["features"];
