af05c7fac26d20e123f3e523f79fef3c15371494
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-21 19:33:58 +0300

    feat(hooks/search): add useSearch hook

---
+++ src/hooks/search/useSearch.ts
@@ -0,0 +1,39 @@
+import { FormEvent, useCallback } from "react";
+import { useSearchParams } from "react-router-dom";
+
+export function useSearch() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const search = searchParams.get("q");
+  const selectedCategories = searchParams.get("categories");
+  const selectedRadius = searchParams.get("radius");
+  const isMissingParams = !search || !selectedCategories || !selectedRadius;
+
+  const onSubmit = useCallback(
+    (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      const formData = new FormData(event.currentTarget);
+      const q = formData.get("q") as string;
+      const categories = formData.get("categories") as string;
+      const radius = formData.get("radius") as string;
+
+      setSearchParams({
+        q,
+        categories,
+        radius,
+      });
+    },
+    [setSearchParams],
+  );
+
+  return {
+    params: {
+      search,
+      selectedCategories,
+      selectedRadius,
+    },
+    isMissingParams,
+    onSubmit,
+  };
+}
