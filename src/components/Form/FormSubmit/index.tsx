508ca7db8eb3d7fa5179b43ec5e83e72abc491e3
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-22 23:42:11 +0300

    refactor(components): update FormSubmit props
    Make inline object inside PropsWithChildren

--- src/components/Form/FormSubmit/index.tsx
+++ src/components/Form/FormSubmit/index.tsx
@@ -2,14 +2,10 @@ import { PropsWithChildren, memo } from "react";
 
 import { Spinner } from "@/components/Spinner";
 
-interface FormSubmitProps extends PropsWithChildren {
-  isLoading?: boolean;
-}
-
 export const FormSubmit = memo(function FormSubmit({
   children,
   isLoading = false,
-}: FormSubmitProps) {
+}: PropsWithChildren<{ isLoading?: boolean }>) {
   return (
     <button type="submit" className="btn btn-primary" disabled={isLoading}>
       {isLoading && <Spinner />}
