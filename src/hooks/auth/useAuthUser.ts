d0b3c28a5d17d774d58e646eb9ae5f22b6b19352
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-23 19:08:38 +0300

    feat(hooks): add new map and auth hooks
    useAuthUser: Subscribes to `onAuthStateChanged` and returns `isFetching`, `userData` and `handleLogout`
    useLocationFetch: Fetches places on location change and returns `isLoading` and `error`

---
+++ src/hooks/auth/useAuthUser.ts
@@ -0,0 +1,36 @@
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
+
+import { useAppDispatch } from "@/hooks/redux/useAppDispatch";
+import { useAppSelector } from "@/hooks/redux/useAppSelector";
+
+import { auth } from "@/services/firebase/app";
+
+import { resetUser, setUser } from "@/store/user";
+
+export function useAuthUser() {
+  const [isFetching, setIsFetching] = useState(true);
+  const { userData } = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) return dispatch(resetUser());
+      const { uid, email, displayName } = user;
+      dispatch(setUser({ uid, email, displayName }));
+      setIsFetching(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return { userData, isFetching, handleLogout };
+}
---
+++ src/hooks/map/useLocationFetch.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+import { useAppDispatch } from "@/hooks/redux/useAppDispatch";
+import { useAppSelector } from "@/hooks/redux/useAppSelector";
+
+import { getLocationPlaces } from "@/services/geoapify/getLocationPlaces";
+
+import { setPlaces } from "@/store/places";
+
+export function useLocationFetch() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+  const {
+    locationCoordinates: { lat, lng },
+  } = useAppSelector((state) => state.location);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    getLocationPlaces(lat, lng)
+      .then((places) => dispatch(setPlaces(places)))
+      .catch((error) => setError(error))
+      .finally(() => setIsLoading(false));
+  }, [dispatch, lat, lng, getLocationPlaces]);
+
+  return { isLoading, error };
+}
