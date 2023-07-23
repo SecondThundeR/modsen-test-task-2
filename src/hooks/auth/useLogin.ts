1322bc5de9c5a70974c355c9bdbb42a1e90becd4
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-24 02:19:51 +0300

    refactor(hooks): update login/signup hooks
    Cleanup and improve onSubmit callback
    Change type of error state to unknown and null

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -9,22 +9,28 @@ import { auth } from "@/services/firebase/app";
 export function useLogin() {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
+  const [error, setError] = useState<unknown | null>(null);
 
   const onSubmit = useCallback(
-    (event: FormEvent<HTMLFormElement>) => {
+    async (event: FormEvent<HTMLFormElement>) => {
       event.preventDefault();
       setError(null);
-      setIsLoading(true);
 
       const formData = new FormData(event.currentTarget);
       const email = formData.get("email") as string;
       const password = formData.get("password") as string;
 
-      signInWithEmailAndPassword(auth, email, password)
-        .then(() => navigate(ROUTES.home))
-        .catch(setError)
-        .finally(() => setIsLoading(false));
+      setIsLoading(true);
+
+      try {
+        await signInWithEmailAndPassword(auth, email, password);
+        navigate(ROUTES.home);
+      } catch (error) {
+        console.error(error);
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
     },
     [navigate, setError, setIsLoading],
   );
--- src/hooks/auth/useSignup.ts
+++ src/hooks/auth/useSignup.ts
@@ -10,36 +10,44 @@ import { auth, database } from "@/services/firebase/app";
 export function useSignup() {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
+  const [error, setError] = useState<unknown | null>(null);
 
   const onSubmit = useCallback(
     async (event: FormEvent<HTMLFormElement>) => {
       event.preventDefault();
       setError(null);
-      setIsLoading(true);
 
       const formData = new FormData(event.currentTarget);
       const email = formData.get("email") as string;
       const password = formData.get("password") as string;
       const displayName = formData.get("displayName") as string;
 
-      createUserWithEmailAndPassword(auth, email, password)
-        .then((credentials) => {
-          updateProfile(credentials.user, {
-            displayName,
-          });
-          return credentials;
-        })
-        .then((credentials) => {
-          const { uid, email } = credentials.user;
-          set(ref(database, "users/" + uid), {
-            username: displayName,
-            email: email,
-          });
-        })
-        .then(() => navigate(ROUTES.home))
-        .catch(setError)
-        .finally(() => setIsLoading(false));
+      setIsLoading(true);
+
+      try {
+        const credentials = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password,
+        );
+
+        const { user } = credentials;
+
+        await updateProfile(user, {
+          displayName,
+        });
+
+        await set(ref(database, "users/" + user.uid), {
+          username: displayName,
+        });
+
+        navigate(ROUTES.home);
+      } catch (error) {
+        console.error(error);
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
     },
     [navigate, setError, setIsLoading],
   );
