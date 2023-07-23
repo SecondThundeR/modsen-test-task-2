250f93d0f85ce809ca61437f3b79d4ee6463cef1
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-24 02:35:49 +0300

    refactor(services/geoapify): update `getPlaceDetails`
    Refactor method to be more modular

--- src/services/geoapify/getPlaceDetails.ts
+++ src/services/geoapify/getPlaceDetails.ts
@@ -4,15 +4,19 @@ import { PlaceDetailsSchema } from "@/schemas/geoapify";
 
 import { fetcher } from "@/services/geoapify/fetcher";
 
+import { parseResponse } from "@/utils/zod/parseResponse";
+
+const buildParams = (placeId: string) => {
+  return {
+    id: placeId,
+    features: "details",
+    lang: "ru",
+    apiKey: import.meta.env.VITE_GEOAPIFY_KEY,
+  };
+};
+
 export const getPlaceDetails = async (placeId: string) => {
-  const res = await fetcher.get(PLACE_DETAILS_ENDPOINT, {
-    params: {
-      id: placeId,
-      features: "details",
-      lang: "ru",
-      apiKey: import.meta.env.VITE_GEOAPIFY_KEY,
-    },
-  });
-  const parsedRes = await PlaceDetailsSchema.parseAsync(res.data);
-  return parsedRes.features;
+  const params = buildParams(placeId);
+  const res = await fetcher.get(PLACE_DETAILS_ENDPOINT, { params });
+  return (await parseResponse(res.data, PlaceDetailsSchema)).features;
 };
