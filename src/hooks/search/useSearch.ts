23fd599822b800ee9eaf0635d37fb79d88cfd716
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-24 02:45:12 +0300

    feat(hooks/search): convert radius to meters in searchParams

--- src/hooks/search/useSearch.ts
+++ src/hooks/search/useSearch.ts
@@ -22,7 +22,7 @@ export function useSearch() {
       setSearchParams({
         q,
         categories,
-        radius,
+        radius: String(+radius * 1000),
       });
     },
     [setSearchParams],
