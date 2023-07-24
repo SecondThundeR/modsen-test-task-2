c882ff321629e25bda98645d0effa0504e72da7f
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-24 12:55:00 +0300

    refactor: remove support for Geoapify's Places Details

--- src/constants/geoapify/links.ts
+++ src/constants/geoapify/links.ts
@@ -1,3 +1,2 @@
 const BASE_URL = "https://api.geoapify.com/v2";
 export const PLACES_ENDPOINT = `${BASE_URL}/places`;
-export const PLACE_DETAILS_ENDPOINT = `${BASE_URL}/place-details`;
--- src/schemas/geoapify.ts
+++ src/schemas/geoapify.ts
@@ -22,28 +22,5 @@ export const PlacesSchema = z.object({
   ),
 });
 
-const PlaceDetailsPropertiesSchema = PlacesPropertiesSchema.merge(
-  z.object({
-    website: z.string(),
-    description: z.string(),
-    contact: z.object({
-      phone: z.string(),
-    }),
-  }),
-);
-
-export const PlaceDetailsSchema = z.object({
-  features: z.array(
-    z.object({
-      properties: PlaceDetailsPropertiesSchema,
-    }),
-  ),
-});
-
 export type PlacesPropeties = z.infer<typeof PlacesPropertiesSchema>;
 export type Places = z.infer<typeof PlacesSchema>["features"];
-
-export type PlaceDetailsPropeties = z.infer<
-  typeof PlaceDetailsPropertiesSchema
->;
-export type PlaceDetails = z.infer<typeof PlaceDetailsSchema>["features"];
