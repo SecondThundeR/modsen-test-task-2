eca9ffb33faa18e31cac9661ffbc18a4f36c1e45
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-26 11:12:07 +0300

    feat(vite.config): add removal of legal comments
    Bundle size:
    - Before change: 886kb
    - After change: 774kb (112kb less)

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import path from "path";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
@@ -18,6 +17,9 @@ export default defineConfig({
       "@/store": path.resolve(__dirname, "./src/store"),
       "@/utils": path.resolve(__dirname, "./src/utils"),
     },
+  },
+  esbuild: {
+    legalComments: "none",
   },
   build: {
     chunkSizeWarningLimit: 800,
