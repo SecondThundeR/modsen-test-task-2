46df862f43dbaea586f2d89db3f48cce4b7801be
SecondThundeR <awayfromgalaxy@gmail.com>
2023-07-27 14:58:58 +0300

    fix(components): destructure `categoryInfo` in `CategoryIcons`

--- src/components/ui/CategoryIcons/index.tsx
+++ src/components/ui/CategoryIcons/index.tsx
@@ -9,18 +9,16 @@ interface CategoryIconsProps {
 export const CategoryIcons = memo(function CategoryIcons({
   category,
 }: CategoryIconsProps) {
-  const categoryInfo = categoriesMapping[category];
+  const { name, url } = categoriesMapping[category];
 
   return (
-    categoryInfo && (
-      <img
-        width={36}
-        height={36}
-        className="-mr-4 rounded-full border-4 border-base-100"
-        src={categoryInfo.url}
-        alt={categoryInfo.name}
-        title={categoryInfo.name}
-      />
-    )
+    <img
+      width={36}
+      height={36}
+      className="-mr-4 rounded-full border-4 border-base-100"
+      src={url}
+      alt={name}
+      title={name}
+    />
   );
 });
